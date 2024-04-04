7ecadbf3ad4c032cc7c7c198b8335d094cd29c11
danielbrog <brog.daniel@gmail.com>
2024-04-04 17:53:08 -0400

    layout set

---
+++ graveyardApp/src/components/Grave.js
@@ -0,0 +1,12 @@
+import React from "react";
+import '../styles/components/_grave.scss'
+
+const Grave= (props) => {
+    console.log(props.id)
+    return (
+        <div className="grave" >
+        </div>
+    );
+}
+
+export default Grave;
--- graveyardApp/src/components/GraveyardLayout.js
+++ graveyardApp/src/components/GraveyardLayout.js
@@ -1,13 +1,26 @@
 import React from "react";
 import '../styles/components/_graveyardlayout.scss'
+import Grave from "./Grave";
+
+const ColumnOfGrave = (props) => {
+
+    const graves = [];
+
+    for (let i=0;i<17;i++) {
+
+        for (let j=0;j<8;j++) {
+            const id= "grave "+i+ " " +j
+            graves.push(<Grave key={id} />)
+        }
+    }
+
+    return (graves)
+}
 
 const GraveyardLayout= (props) => {
     return (
         <div className="graveyard__layout">
-            <div className="graveyard__graveSlot"></div>
-            <div className="graveyard__graveSlot"></div>
-            <div className="graveyard__graveSlot"></div>
-            <div className="graveyard__graveSlot"></div>
+            <ColumnOfGrave />
         </div>
     );
 }
