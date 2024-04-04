b880779989074524d6add0f2bd6198eb9d146732
danielbrog <brog.daniel@gmail.com>
2024-04-04 17:56:14 -0400

    removed unneeded component in Layout

--- graveyardApp/src/components/GraveyardLayout.js
+++ graveyardApp/src/components/GraveyardLayout.js
@@ -2,25 +2,25 @@ import React from "react";
 import '../styles/components/_graveyardlayout.scss'
 import Grave from "./Grave";
 
-const ColumnOfGrave = (props) => {
-
-    const graves = [];
-
-    for (let i=0;i<17;i++) {
-
-        for (let j=0;j<8;j++) {
-            const id= "grave "+i+ " " +j
-            graves.push(<Grave key={id} />)
-        }
-    }
-
-    return (graves)
-}
 
 const GraveyardLayout= (props) => {
+
+    const createGraves= () => {
+        const graves = [];
+
+        for (let i=0;i<17;i++) {
+            for (let j=0;j<8;j++) {
+                const id= "grave "+i+ " " +j
+                graves.push(<Grave key={id} />)
+            }
+        }
+        return (graves)
+    }
+
+
     return (
         <div className="graveyard__layout">
-            <ColumnOfGrave />
+            {createGraves()}
         </div>
     );
 }
