21f5e629b8166c48ca86e704a060e9ecc046bb10
danielbrog <brog.daniel@gmail.com>
2024-04-05 15:58:04 -0400

    updated layout with walls and state

--- graveyardApp/src/components/Grave.js
+++ graveyardApp/src/components/Grave.js
@@ -2,9 +2,15 @@ import React from "react";
 import '../styles/components/_grave.scss'
 
 const Grave= (props) => {
-    console.log(props.id)
+
+    const handleClick = () => {
+        console.log('X: ' + props.x)
+        console.log('Y: ' + props.y)
+        console.log('isWall: ' + props.grave.isWall)
+    }
+
     return (
-        <div className="grave" >
+        <div className={props.grave.isWall?'grave_wall':'grave'} onClick = {handleClick} iswall={props.grave.isWall}>
         </div>
     );
 }
